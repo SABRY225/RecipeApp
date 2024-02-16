2945de3e7d4d672df632723f26af170878dfa3e1
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-02-16 18:59:00 +0200

    add Deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index="true" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
       </Routes>
