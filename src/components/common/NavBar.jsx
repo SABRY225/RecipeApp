ff52b2a9296f7d3faacd54b2c2759d1bc2757f4e
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-02-16 19:02:14 +0200

    add Deploy

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Menu.Item>
         <img src={logo} alt="logo" style={{ width: 60 }} />
       </Menu.Item>
-      <Menu.Item name="Home" as={Link} to="/" />
+      <Menu.Item name="Home" as={Link} to="" />
       <Menu.Item name="Recipes" as={Link} to="/recipes" />    
     
 
